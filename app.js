9da99f4f5685b96b498ea7e0fc8f9b815c6f79e2
proNeel02 <prashantdabe02@gmail.com>
2022-12-27 17:27:30 +0530

    duration

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
+
+
+
+var vid = document.getElementById("myVideo");
+console.log("vid = ",vid);
+
+// console.log(vid.duration);
+let carVideo = document.getElementById("carVideo");
+
+
 document.addEventListener("DOMContentLoaded", () => {
     window.setTimeout(function() {
       document.getElementById("Starting").style.display = "none";
-      document.getElementById("StartingContent").style.display = "none";
-    }, 7000);
+      carVideo.play();
+      document.getElementById("StartingContent").style.display = "none";     
+
+    },8700);
   });
 
 
@@ -13,5 +25,4 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   document.getElementById("skip").addEventListener("click", skip);
-
 
