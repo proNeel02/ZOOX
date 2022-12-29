1877bfc20664a7d06bc850e62003431b14a9d98b
proNeel02 <prashantdabe02@gmail.com>
2022-12-29 16:04:50 +0530

    hey

--- app.js
+++ app.js
@@ -1,28 +1,57 @@
 
-
-
-var vid = document.getElementById("myVideo");
-console.log("vid = ",vid);
-
-// console.log(vid.duration);
+// cathing necessary HTML elements (in this case video) by using id
+let vid = document.getElementById("myVideo");
 let carVideo = document.getElementById("carVideo");
 
+let carPresentation = document.getElementById("carPresentation");
 
-document.addEventListener("DOMContentLoaded", () => {
-    window.setTimeout(function() {
-      document.getElementById("Starting").style.display = "none";
-      carVideo.play();
-      document.getElementById("StartingContent").style.display = "none";     
+carPresentation.style.display = "none";
+/// This  is perfectly matching first video ending and next is video statring
+//  this is pefect code for this satisfied with this accurate trasition ...... :)
+vid.addEventListener("ended", myHandler, false);
+function myHandler(e) {
+  carPresentation.style.display = "block";
+  carVideo.style.display = "block";
+  document.getElementById("Starting").style.display = "none";
+  document.getElementById("StartingContent").style.display = "none";
+  carVideo.play();
+}
 
-    },8700);
-  });
+//  nearly perfect playing both videos one after other but not satisfied by this
+// not matching exact finishing......look for good...    :)
+// document.addEventListener("DOMContentLoaded", () => {
+//   vid.onloadedmetadata = function () {
+//     videoDurationIs = this.duration;
+//     console.log("videoDurationIs(Inside)", videoDurationIs);
+
+//     window.setTimeout(function () {
+//       document.getElementById("Starting").style.display = "none";
+//       document.getElementById("StartingContent").style.display = "none";
+//       carVideo.play();
+//     }, videoDurationIs * 1000);
+//   };
+// });
+
+function skip() {
+  carPresentation.style.display = "block";
+  document.getElementById("Starting").style.display = "none";
+  document.getElementById("StartingContent").style.display = "none";
+  carVideo.play();
+}
+
+let btn = document.getElementById("skip");
+btn.addEventListener("click", skip);
+
+let header = document.getElementById("header");
+let body = document.getElementsByTagName("body");
 
 
-  
-  function skip() {
-    document.getElementById("Starting").style.display = "none";
-    document.getElementById("StartingContent").style.display = "none";
-  }
+function fixedheader(){
+  console.log("before");
+  header.style.position = "fixed";
+  console.log("after");
+}
 
-  document.getElementById("skip").addEventListener("click", skip);
+
+body.addEventListener("scroll", fixedheader);
 
